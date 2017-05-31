c56d4255c4a446866183d3a14f6376ff2d699416
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-05-31 14:11:51 +0530

    Update install_webmail.sh

--- distros/ubuntu-16.04/install_webmail.sh
+++ distros/ubuntu-16.04/install_webmail.sh
@@ -4,19 +4,6 @@
 #---------------------------------------------------------------------
 InstallWebmail() {
   echo -n "Installing webmail client ($CFG_WEBMAIL)... "
-  case $CFG_WEBMAIL in
-	"roundcube")
-	  CFG_ROUNDCUBE_PWD=$(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c12)
-	  echo "roundcube-core roundcube/dbconfig-install boolean true" | debconf-set-selections
-	  echo "roundcube-core roundcube/database-type select mysql" | debconf-set-selections
-	  echo "roundcube-core roundcube/mysql/admin-pass password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-	  echo "roundcube-core roundcube/db/dbname string roundcube" | debconf-set-selections
-	  echo "roundcube-core roundcube/mysql/app-pass password $CFG_ROUNDCUBE_PWD" | debconf-set-selections
-	  echo "roundcube-core roundcube/app-password-confirm password $CFG_ROUNDCUBE_PWD" | debconf-set-selections
-	  echo "roundcube-core roundcube/hosts string localhost" | debconf-set-selections
-	  apt-get -yqq install roundcube roundcube-core roundcube-mysql roundcube-plugins roundcube-plugins-extra javascript-common libjs-jquery-mousewheel php-net-sieve tinymce > /dev/null 2>&1
- 
-
   echo "dictionaries-common dictionaries-common/default-wordlist select american (American English)" | debconf-set-selections
   apt-get -yqq install squirrelmail wamerican > /dev/null 2>&1
   ln -s /etc/squirrelmail/apache.conf /etc/apache2/conf-enabled/squirrelmail.conf
@@ -37,18 +24,18 @@ InstallWebmail() {
 		"dovecot")
 		  sed -i 's/$imap_server_type       = "other";/$imap_server_type       = "dovecot";/' /etc/squirrelmail/config.php
 		  sed -i 's/$trash_folder                   = "INBOX.Trash";/$trash_folder                   = "Trash";/' /etc/squirrelmail/config.php
-		  sed -i 's/$sent_folder                    = "INBOX.Sent";/$sent_folder                    = "Sent";/' /etc/squirrelmail/config.php#		  sed -i 's/$draft_folder                   = "INBOX.Drafts";/$draft_folder                   = "Drafts";/' /etc/squirrelmail/config.php
+		  sed -i 's/$sent_folder                    = "INBOX.Sent";/$sent_folder                    = "Sent";/' /etc/squirrelmail/config.php
+		  sed -i 's/$draft_folder                   = "INBOX.Drafts";/$draft_folder                   = "Drafts";/' /etc/squirrelmail/config.php
 		  sed -i 's/$default_sub_of_inbox           = true;/$default_sub_of_inbox           = false;/' /etc/squirrelmail/config.php
 		  sed -i 's/$delete_folder                  = false;/$delete_folder                  = true;/' /etc/squirrelmail/config.php
 		  ;;
 	  esac
- mkdir /var/lib/squirrelmail/tmp
+  mkdir /var/lib/squirrelmail/tmp
   chown www-data /var/lib/squirrelmail/tmp
- if [ "$CFG_WEBSERVER" == "apache" ]; then
+  if [ "$CFG_WEBSERVER" == "apache" ]; then
 	  service apache2 restart > /dev/null 2>&1
   else
 	  service nginx restart > /dev/null 2>&1
   fi
   echo -e "[${green}DONE${NC}]\n"
 }
-
